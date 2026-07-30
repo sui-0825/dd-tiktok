408e958565832927c4e33e608ccf6e801be9ae3e
sui-0825 <ku0121c@gmail.com>
2026-07-30 13:29:28 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='dd-tiktok-v17-5-4';
+const CACHE='dd-tiktok-v18-0';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./app-icon.svg',
   './apple-touch-icon.png',
   './app-icon-192.png',
