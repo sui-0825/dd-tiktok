a8aff098c5471630bf485af8f7a7be4d633a6c45
sui-0825 <ku0121c@gmail.com>
2026-07-31 08:47:55 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,11 @@
-// Ver22.0: 更新優先。旧キャッシュを削除してService Workerを解除します。
-self.addEventListener('install', () => self.skipWaiting());
+// Ver24.3 - no offline cache; always update from GitHub Pages
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys()
-      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
       .then(() => self.registration.unregister())
       .then(() => self.clients.claim())
   );
